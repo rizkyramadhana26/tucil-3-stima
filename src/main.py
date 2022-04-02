b0d9df01fd761c54f3d9f9b90b4ed762ae14ee57
Rizky Ramadhana <rizkyramadhana26@gmail.com>
2022-04-02 15:15:20 +0700

    menambahkan random generator

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 from Node import *
 from BranchNBound import *
 
-filename = input("Masukkan nama file (harus berada dalam folder test) : ")
+source = input("Ketik 1 untuk menggunakan puzzle random, ketik 2 untuk menggunakan puzzle dari file : ")
+if source=='1':
+    level = input("Ketik level puzzle yang diinginkan (1, 2, atau 3) : ")
+    puzzle = randomPuzzle(level)
+else:
+    filename = input("Masukkan nama file (harus berada dalam folder test) : ")
+    puzzle = readFile("test/"+filename)
 
-puzzle = readFile("test/"+filename)
 bnb = BranchNBound(puzzle)
 
 if bnb.check()==1:
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,6 @@
+from random import randint
+from Node import *
+
 def toString(puzzle):
     return ','.join(str(item) for innerlist in puzzle for item in innerlist)
 
@@ -9,3 +12,43 @@ def readFile(filename):
         for j in range(4):
             lines[i][j] = int(lines[i][j])
     return lines
+
+def randomPuzzle(n):
+    if(n==1):
+        steps=15
+    elif(n==2):
+        steps=30
+    else:
+        steps=60
+
+    root = Node("NO",[[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]],(3,3),0)
+
+    i=0
+    while i<steps :
+        y = randint(1,4)
+        if y==1 :
+            try:
+                root=root.UP()
+                i+=1
+            except:
+                pass
+        elif y==2 :
+            try:
+                root=root.DOWN()
+                i+=1
+            except:
+                pass
+        elif y==3 :
+            try:
+                root=root.LEFT()
+                i+=1
+            except:
+                pass
+        else :
+            try:
+                root=root.RIGHT()
+                i+=1
+            except:
+                pass
+    Node.numOfNodes = 0
+    return root.matrix
